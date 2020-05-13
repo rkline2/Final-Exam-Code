91a17034fb7c3e57b572777e2db60f937800244f
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-05-13 17:39:54 -0400

    version 1.1

--- Robot.cpp
+++ Robot.cpp
@@ -1,65 +1,62 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include <iostream> 
+#include <string> 
+using namespace std; 
 
-class Robot
-{
-public:
-	Robot();
-	Robot(string, int);
-	Robot(const Robot&);
-	~Robot();
+class Robot { 
+public: 
+    Robot(); 
+    Robot(string, int); 
+    Robot(const Robot&); 
+    ~Robot(); 
+  
+    Robot& operator=(const Robot&); 
 
-	Robot& operator=(const Robot&);
+    string GetName(); 
+    void SetName(string); 
 
-	string GetName();
-	void SetName(string);
+    int GetData(); 
+    void SetData(int); 
+private: 
+    string m_name; 
+    int* m_data; 
+}; 
 
-	int GetData();
-	void SetData(int);
-private:
-	string m_name;
-	int* m_data;
-};
+Robot::Robot() { 
+    m_name = ""; 
+    m_data = new int(); 
+} 
 
-Robot::Robot()
-{
-	m_name = "";
-	m_data = new int;
+Robot::Robot(string name, int data) { 
+    m_name = name; 
+    m_data = new int(data); 
+} 
+
+Robot::~Robot() {
+    m_name.clear();
+    delete m_data;  
 }
 
-Robot::Robot(string name, int data) {
-	m_name = name;
-	m_data = new int(data);
-}
+Robot::Robot(const Robot& source){ 
+    m_name = source.m_name;  
+    m_data = new int(*source.m_data);  
+} 
 
-Robot::Robot(const Robot& source){
-	m_name = source.m_name; 
-	m_data = new int(*source.m_data); 
-}
+Robot& Robot::operator=(const Robot& source) { 
+    if (this != &source) { 
+        m_name = source.m_name;  
+        delete m_data; 
+        m_data = new int(*source.m_data); 
+    } 
+    return *this;  
+} 
 
-Robot& Robot::operator=(const Robot& source) {
-	if (this != &source) {
-		m_name = source.m_name; 
-		m_data = new int(*source.m_data);
-	}
-	return *this; 
-}
+string Robot::GetName() { return m_name; } 
 
-Robot::~Robot()
-{
-	delete m_data; 
-}
+void Robot::SetName(string name) { m_name = name; } 
 
-string Robot::GetName() { return m_name; }
+int Robot::GetData() { return *m_data; } 
 
-void Robot::SetName(string name) { m_name = name; }
-
-int Robot::GetData() { return *m_data; }
-
-void Robot::SetData(int data) { m_data = &data; }
-
-int main() {
-
-	return 0;
-}
+void Robot::SetData(int data) {  
+delete m_data; 
+*m_data = data;  
+} 
